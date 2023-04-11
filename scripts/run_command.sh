127c0422a03e6f8fa9619e173144ae8ab053d31c
Hailan Shanbhag <hshanbha@illinois.edu>
2023-04-11 05:43:17 -0500

    a

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# echo "Running train command for similar environments..."
-# python nn/train.py 
+echo "Running train command for similar environments..."
+python nn/train.py 
 
-# echo "Running evaluation metrics..."
-# python metrics/compute_aps_read_name.py
+echo "Running evaluation metrics..."
+python metrics/compute_aps_read_name.py
 
 echo "Running train command for different environments..."
 python nn/train.py --environment different
