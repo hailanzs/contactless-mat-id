a4d80525773e307a3873146064b42e1a7ca11612
hash <hailan.shanbhag@epfl.ch>
2023-05-12 08:48:03 +0200

    a

--- scripts/test_command.sh
+++ scripts/test_command.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 echo "Running commands for material wise classification\n\n" 
 echo "Running train command for similar env materials..."
-python nn/test.py --exp_name main_material_2023_04_23-10_35_57 --lim 30
+python nn/test.py --exp_name main_material --lim 30
 
 echo "Running evaluation metrics for same env materials..."
-python metrics/compute_aps.py --exp_name main_material_2023_04_23-10_35_57 --tested_results 0 
+python metrics/compute_aps.py --exp_name main_material --tested_results 0 
 
 echo "Running test command for different env materials..." 
-python nn/test.py --exp_name main_material_different_2023_04_23-10_48_37 
+python nn/test.py --exp_name main_material_different
 
 echo "Running evaluation metrics from different env materials..."
-python metrics/compute_aps.py --exp_name main_material_different_2023_04_23-10_48_37 --tested_results 0 
+python metrics/compute_aps.py --exp_name main_material_different --tested_results 0 
 
 echo "Running commands for object wise classification\n\n" 
 echo "Running train command for similar env objects..."
-python nn/test.py --exp_name main_objs_2023_04_23-11_04_11
+python nn/test.py --exp_name main_objs
 
 echo "Running evaluation metrics for same env objects..."
-python metrics/compute_aps.py --exp_name main_objs_2023_04_23-11_04_11 --tested_results 0 
+python metrics/compute_aps.py --exp_name main_objs --tested_results 0 
 
 echo "Running test command for different env objects..."
-python nn/test.py --exp_name main_objs_different_2023_04_23-11_06_41
+python nn/test.py --exp_name main_objs_different
 
 echo "Running evaluation metrics from different env objects..."
-python metrics/compute_aps.py --exp_name main_objs_different_2023_04_23-11_06_41 --tested_results 0 
+python metrics/compute_aps.py --exp_name main_objs_different --tested_results 0 
